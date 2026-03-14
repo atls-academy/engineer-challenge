778052719f95c33e4c3f575a42ea5444acbaa521
SashaFlake <34577392+SashaFlake@users.noreply.github.com>
2026-03-14 13:05:20 +0300

    chore: clean up Routing, build.gradle.kts, settings.gradle.kts, gradle.properties after removing core / City / User

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,19 +3,14 @@ rootProject.name = "engineer-challenge"
 pluginManagement {
     repositories {
         gradlePluginPortal()
-        maven("https://redirector.kotlinlang.org/maven/kxrpc-grpc")
     }
 }
 
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
-        maven("https://redirector.kotlinlang.org/maven/kxrpc-grpc")
-        maven("https://packages.confluent.io/maven/")
     }
 }
 
 include(":server")
-include(":core")
 include(":client")
-include(":domain")

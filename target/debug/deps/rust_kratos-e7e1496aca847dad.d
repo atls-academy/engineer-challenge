01233d71df729abd960ccd7293bbaa66cc39bbb4
vwency <veniaminshp@gmail.com>
2026-03-22 16:15:09 +0600

    chore: update submodules

---
+++ target/debug/deps/rust_kratos-e7e1496aca847dad.d
@@ -0,0 +1,5 @@
+/home/vwency/Documents/Github/temp/engineer-challenge/target/debug/deps/rust_kratos-e7e1496aca847dad.d: src/main.rs
+
+/home/vwency/Documents/Github/temp/engineer-challenge/target/debug/deps/librust_kratos-e7e1496aca847dad.rmeta: src/main.rs
+
+src/main.rs:
